080e47986d25ab18e4f0481c540f8d72869cf6b2
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2022-11-18 21:52:50 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+<h2>My Version</h2>
+<div>
+  <p>This is actually getting into my favor for writing code. Its actually pretty awesome.
+  <a href="#!" target="_blank" rel="noopeneer noreferrer">Here is a link</a>.</p>
+</div>
+`;
